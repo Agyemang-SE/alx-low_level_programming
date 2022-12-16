585cbe8ad98bed30625fef1378fb05f0c106a8da
Agyemang-SE <kevin.agyemang@proton.me>
2022-12-16 01:41:36 +0000

    Task completion for debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -  test if the function return 0 for input number of zero
+ *
+ * Return: 0 on Success
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+}
