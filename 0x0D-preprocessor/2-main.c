8507d5b2866733f04bf5e59b7f77bad654bd4ecd
Agyemang-SE <kevin.agyemang@proton.me>
2023-01-17 00:56:49 +0000

    Task Completion

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prints name of a file
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#define SUM(a, b) (a + b)
+
+#endif
