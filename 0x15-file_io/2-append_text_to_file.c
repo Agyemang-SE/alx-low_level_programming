09f93706565e8aeacac763359f7cc95615bd0b4e
Agyemang-SE <kevin.agyemang@proton.me>
2023-10-02 23:23:42 +0000

    Corredtion to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,37 @@
-#include "main.h"
-#include <string.h>
+#include "main.h" 
+  
+/** 
+* append_text_to_file - appends text at the end of a file 
+* @filename: path of the file 
+* @text_content: content to append at end of file 
+* Return: 1 on Success, -1 on failure 
+*/ 
+int append_text_to_file(const char *filename, char *text_content) 
+{ 
+int count, len = 0, fd, i; 
+ 
+if (filename == NULL) 
+return (-1); 
+  
+/* Open file and get file descriptor */ 
+fd = open(filename, 2 | O_APPEND, 0600); 
+if (fd == -1) 
+return (-1); 
+  
+if (text_content == NULL) 
+return (1); 
 
-/**
- * append_text_to_file - function appends text at the end of  a file
- * @filename: pointer to the file structure
- * @text_content: content of the text
- * Return: 1 on success, 0 otherwise
- */
-
-int append_text_to_file(const char *filename, char *text_content)
-{
-int fd;
-ssize_t write_count;
-/* check for invalid inputs */
-if (!filename)
-{
-return (-1);
-}
-if (!text_content)
-{
-return (-1);
-}
-/* open the file in append mode */
-fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
-{
-return (-1);
-}
-/* write the contents of the buffer to the file */
-write_count = write(fd, text_content, strlen(text_content));
-if (write_count == -1)
-{
-close(fd);
-return (-1);
-}
-/* close the file and return 1 for success */
-close(fd);
-return (1);
-}
+for (i = 0; text_content[i]; i++) 
+len++; 
+  
+/* write into file description */ 
+count = write(fd, text_content, len); 
+if (count == -1) 
+{ 
+close(fd); 
+return (-1); 
+} 
+close(fd); 
+  
+return (1); 
+ }
