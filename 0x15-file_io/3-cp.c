286b27c7c3853f11096fd3c21225d4bb7286ccff
Agyemang-SE <kevin.agyemang@proton.me>
2023-10-02 23:51:07 +0000

    Correction to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,69 +1,114 @@
 #include "main.h"
 
 /**
-*read_file - function reads the file_to
-*@file_from: pointer to the file structure
-*/
-void read_file(const char *file_from)
-{
-int fd_from;
-/* if open file fails dprintf to STDERR_FILENO */
-fd_from = open(file_from, O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-exit(98);
-}
-close(fd_from);
-}
-/**
- * cp_file - Copy the content of a file to another file
- * @file_from: File to copy from
- * @file_to: File to copy to
+ * close_file - close file and check if was succesfull
+ * @fd: file descriptor
  */
-void cp_file(const char *file_from, const char *file_to)
+void close_file(int fd)
 {
-int fd_from, fd_to;
-char buf[BUFSIZE];
-ssize_t read_count, write_count;
-/* if reading and writing file fail, dprintf into STDERR_FILENO */
-fd_from = open(file_from, O_RDONLY);
-if (!fd_from)
+if (close(fd) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-exit(98);
-}
-fd_to = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR |
-S_IRGRP | S_IWGRP | S_IROTH);
-if (!fd_to)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-close(fd_from);
-exit(99);
-}
-read_count = read(fd_from, (void *)buf, BUFSIZE);
-write_count =  write(fd_to, (void *)buf, BUFSIZE);
-while (read_count > 0)
-{
-if (write_count == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-close(fd_from);
-close(fd_to);
-exit(99);
-}
-}
-if (read_count == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
-if (close(fd_from) == -1 || close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-fd_from == -1 ? fd_from : fd_to);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
+}
+
+/**
+ * error_handler - returns respective exit status
+ * @fd: file descriptor
+ * @fname: file descriptor
+ * @type: type of sys call
+ * @buffer: buffer to be free if exit status
+ * Return: 0 on success, exit status on error
+ */
+int error_handler(int fd, char *fname, int type, char *buffer)
+{
+int status = 0;
+(void)fd;
+
+switch (type)
+{
+case READ_ERROR:
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", fname);
+if (buffer != NULL)
+free(buffer);
+status = 98;
+break;
+case WRITE_ERROR:
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", fname);
+if (buffer != NULL)
+free(buffer);
+status = 99;
+break;
+default:
+break;
+}
+return (status);
+}
+
+/**
+ * create_buffer - allocates a buffer of BUFFERSIZE
+ * @fileName: file dest to print error
+ * Return: pointer to new allocated buffer
+ */
+char *create_buffer(char *fileName)
+{
+char *buffer;
+
+buffer = malloc(sizeof(char) * BUFFERSIZE);
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileName);
+exit(99);
+}
+return (buffer);
+}
+
+/**
+ * main - copies contents of 1 file to another
+ * @argc: number of parameters
+ * @argv: array of string parameters
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+int fd_src, fd_dest, res;
+char *buffer;
+
+(void)argc;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+fd_src = open(argv[1], O_RDONLY);
+if (fd_src < -1)
+exit(error_handler(fd_src, argv[1], READ_ERROR, NULL));
+
+buffer = create_buffer(argv[2]);
+
+umask(0);
+fd_dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+if (fd_dest < -1)
+exit(error_handler(fd_dest, argv[2], WRITE_ERROR, buffer));
+
+do {
+res = read(fd_src, buffer, BUFFERSIZE);
+if (res == -1)
+exit(error_handler(res, argv[1], READ_ERROR, buffer));
+res = write(fd_dest, buffer, res);
+if (res == -1)
+exit(error_handler(res, argv[2], WRITE_ERROR, buffer));
+fd_dest = open(argv[2], O_RDWR | O_APPEND);
+if (fd_dest == -1)
+			exit(error_handler(fd_dest, argv[2], WRITE_ERROR, buffer));
+
+} while (res > 0);
+close_file(fd_src);
+close_file(fd_dest);
+free(buffer);
+return (0);
 }
